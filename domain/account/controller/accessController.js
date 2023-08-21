cffa9045abd098d556529b4ca66713ce19b1c878
koldin108902 <alex108902@naver.com>
2023-08-21 16:31:24 +0900

    Feat: 에러 처리 함수 연결

--- domain/account/controller/accessController.js
+++ domain/account/controller/accessController.js
@@ -1,37 +1,46 @@
+const exceptionHandler = require('../../../global/error/exceptionHandler')
 const NotEngoughArgsException = require('../../../global/error/exception/NotEnoughArgsException')
 const accountService = require('../service/accountService')
 const app = require('express').Router()
 
 app.post("/join", async (req, res, next) => {
-    const { password, email, first_name } = req.body
-
-    if (email == undefined || email == null ||
-        password == undefined || password == null) {
-        throw new NotEngoughArgsException()
+    try {
+        const { password, email, first_name } = req.body
+    
+        if (email == undefined || email == null ||
+            password == undefined || password == null) {
+            throw new NotEngoughArgsException()
+        }
+    
+        await accountService.join(email, first_name, password)
+    
+        res.send(JSON.stringify({
+            status: "success",
+            message: "회원가입 완료",
+        }))
+    } catch(e) {
+        exceptionHandler(res, e)
     }
-
-    await accountService.join(email, first_name, password)
-
-    res.send(JSON.stringify({
-        status: "success",
-        message: "회원가입 완료",
-    }))
 })
 
-app.post("login", async (req, res) => {
-    const { email, password } = req.body
-
-    if (!email || !password) throw new NotEngoughArgsException()
-
-    const userId = await accountService.login(email, password)
-    req.session.userId = userId
-
-    res.cookie("sessionID", req.sessionID, { httpOnly: true, secure: false, maxAge: 600000 })
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    res.send(JSON.stringify({
-        message: "로그인 성공",
-        status: "success"
-    }))
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+    
+        if (!email || !password) throw new NotEngoughArgsException()
+    
+        const userId = await accountService.login(email, password)
+        req.session.userId = userId
+    
+        res.cookie("sessionID", req.sessionID, { httpOnly: true, secure: false, maxAge: 600000 })
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.send(JSON.stringify({
+            message: "로그인 성공",
+            status: "success"
+        }))
+    } catch(e) {
+        exceptionHandler(res, e)
+    }
 })
 
 app.post('/logout', async (req, res) => {
