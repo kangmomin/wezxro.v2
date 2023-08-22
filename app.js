454b8617602498e60fd1300a539267049abd16d5
koldin108902 <alex108902@naver.com>
2023-08-23 05:03:45 +0900

    Feat: 서버 시작시 변경사항 적용

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get('*', (req, res) => res.render('error', { status: 404, title: "THE PAGE",
 
 
 //force : 서버 실행 시 마다 테이블을 재생성 할 것인지 아닌지
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({alter: true}).then(() => {
     console.log('DB Sync complete.');
     app.listen(port, () => { console.log(`server is running on ${port}`) })
 })
