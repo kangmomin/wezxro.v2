4969a0b7add305f73db36e7d5dd7ce42fe7dd80f
koldin108902 <alex108902@naver.com>
2023-08-28 19:54:14 +0900

    Fix: alter 옵션 삭제
    인덱스를 계속 생성해서 인덱스가 너무 많아짐

--- app.js
+++ app.js
@@ -61,24 +61,22 @@ app.use((_,__,next) => customMiddleware(next, app, "service"))
 app.use(serviceRouter)
 
 
-// ================== index 페이지 렌더링 =====================
-app.use((_, __, n) => {app.set('views', __dirname + '/global/view'); n()})
-app.get('/', (req, res) => res.status(200).render('index', {
+// ================== index 페이지 렌더링 ===================== 
+app.get('/', (req, res) => res.status(200).render('/global/view/index.ejs', {
     isLogin: req.session.userId !== undefined && req.session.userId !== null
 }))
 // ================== index 페이지 렌더링 =====================
-
+    
 // public static 파일 설정
 app.use(express.static(__dirname + '/public'))
 
 // ================== error 페이지 렌더링 =====================
-app.use((_, __, n) => {app.set('views', __dirname + '/global/view'); n()})
-app.get('*', (req, res) => res.render('error', { status: 404, title: "THE PAGE", content: "WAS NOT FOUND" }))
+app.get('*', (req, res) => res.render('/global/view/error.ejs', { status: 404, title: "THE PAGE", content: "WAS NOT FOUND" }))
 // ================== error 페이지 렌더링 =====================
 
 
 //force : 서버 실행 시 마다 테이블을 재생성 할 것인지 아닌지
-sequelize.sync({alter: true}).then(() => {
+sequelize.sync({}).then(() => {
     console.log('DB Sync complete.');
     app.listen(port, () => { console.log(`server is running on ${port}`) })
 })
