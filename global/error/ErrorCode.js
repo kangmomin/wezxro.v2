e3cc6f8b4eaa261472df251b26b2c68d5bc8fd1a
koldin108902 <alex108902@naver.com>
2023-09-01 11:19:38 +0900

    Refecotr: 에러 코멘트 수정

--- global/error/ErrorCode.js
+++ global/error/ErrorCode.js
@@ -1,7 +1,7 @@
 module.exports = {
     NOT_LOGIN: "로그인이 필요합니다.",
     ACCOUNT_NOT_FOUND: "계정을 찾을 수 없습니다.",
-    USER_NOT_FOUND: "아이디 혹은 계정을 찾을 수 없습니다.",
+    USER_NOT_FOUND: "아이디 혹은 비밀번호를 찾을 수 없습니다.",
     NOT_ENOUGH_ARGS: "필수 데이터가 채워지지 않았습니다.",
     PROVIDER_NOT_FOUND: "도매처를 찾을 수 없습니다.",
     UNKNOWN_PROVIDER: "도매처 정보가 잘못되었습니다.",
