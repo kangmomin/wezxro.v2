24047cf55654cd1de3ece0ff0f2ec037b162b6d7
koldin108902 <alex108902@naver.com>
2023-09-13 15:06:52 +0900

    Refector: DB 변경

--- global/config/config.js
+++ global/config/config.js
@@ -6,12 +6,18 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    dialect: 'mysql',
+    dialect: 'postgres',
     dialectOptions: { // 추가 설정
         charset: 'utf8mb4',
         dateStrings: true,
         typeCast: true,
     },
     timezone: '+09:00',
+    pool: {
+      max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
   },
 };
