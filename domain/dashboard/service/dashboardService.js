3d4f319c9fc9cc05d4403d72c8db3573a307ca3a
koldin108902 <alex108902@naver.com>
2023-09-27 06:13:03 +0900

    Feat: user dashboard

--- domain/dashboard/service/dashboardService.js
+++ domain/dashboard/service/dashboardService.js
@@ -1,7 +1,9 @@
 const ex = module.exports = {}
+const { Op, literal } = require('sequelize')
 const Account = require('../../account/entity/account')
 const NeedLoginException = require('../../account/exception/NeedLoginException')
 const Order = require('../../order/entity/order')
+const sequelize = require('../../../global/config/getSequelize')()
 
 ex.userDetails = async (userId = null) => {
     if (userId === null) throw new NeedLoginException()
@@ -19,9 +21,51 @@ ex.userDetails = async (userId = null) => {
         where: { userId }
     }))?.toLocaleString() || 0
 
+    const orderStatus = {} 
+
+    await Order.findAll({
+        where: { userId },
+        attributes: ["status", [sequelize.fn("COUNT", sequelize.col("status")), "count"]],
+        group: ["status"],
+        raw: true
+    }).then(orders => orders.forEach(val => {
+        orderStatus[val.status] = val.count
+    }))
+
+    const endDate = getLastWeekDate()
+    const truncWithIntervalLiteral = literal("DATE_TRUNC('day', \"updatedAt\")::date");
+
+    const orderDetails = (await Order.findAll({
+        where: { 
+            userId,
+            updatedAt: {
+                [Op.gte]: endDate,
+            }
+         },
+        attributes: [
+            "status", 
+            [truncWithIntervalLiteral, "dayly"],
+            [sequelize.fn('COUNT', sequelize.fn('DISTINCT', sequelize.col('updatedAt'))), 'count'],
+        ],
+        group: ["dayly", "status"],
+        raw: true
+    }))
+
     return {
         userDetails,
         totalCharge,
-        totalOrder
+        totalOrder,
+        orderStatus,
+        orderDetails
+    }
+
+    function getLastWeekDate() {
+        const d = new Date()
+
+        const year = d.getFullYear() // 년
+        const month = d.getMonth() // 월
+        const day = d.getDate() // 일
+        const endDate = new Date(year, month, day - 7).toLocaleDateString()
+        return endDate
     }
 }
